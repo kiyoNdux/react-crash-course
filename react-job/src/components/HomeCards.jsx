1359efe75187230c2ad62f874ccaa28a932386a8
eigenlambda123 <rmvilla987@gmail.com>
2025-09-23 16:04:49 +0800

    created Card wrapper for the HomeCards components

--- react-job/src/components/HomeCards.jsx
+++ react-job/src/components/HomeCards.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import Card from './Card';
 
 function HomeCards() {
   return (
     <section className="py-4">
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-          <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+          <Card>
             <h2 className="text-2xl font-bold">For Developers</h2>
             <p className="mt-2 mb-4">
               Browse our React jobs and start your career today
@@ -16,8 +16,8 @@ function HomeCards() {
             >
               Browse Jobs
             </a>
-          </div>
-          <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
+          </Card>
+          <Card bg="bg-indigo-100">
             <h2 className="text-2xl font-bold">For Employers</h2>
             <p className="mt-2 mb-4">
               List your job to find the perfect developer for the role
@@ -27,8 +27,8 @@ function HomeCards() {
               className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
             >
               Add Job
-            </a>
-          </div>
+            </a>    
+          </Card>
         </div>
       </div>
     </section>
