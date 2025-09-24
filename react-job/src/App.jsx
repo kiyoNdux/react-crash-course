2a44a2d16f6abacad4dd60f9c44596ec65dff985
eigenlambda123 <rmvilla987@gmail.com>
2025-09-24 17:48:45 +0800

    switch <a> tags to Links

--- react-job/src/App.jsx
+++ react-job/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={ <HomePage /> } />
+      <Route path="/jobs" element={ <JobsPage /> } />
     </Route>
   )
 );
