6bbbaba92376e2d0159abda091ad1a1dfd06905b
eigenlambda123 <rmvilla987@gmail.com>
2025-09-24 16:53:09 +0800

    Setup router in App.jsx, create a pages directory and store the HomePage.jsx there

--- react-job/src/App.jsx
+++ react-job/src/App.jsx
@@ -1,23 +1,14 @@
-import React from 'react'
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route index element={ <HomePage /> } />
+  )
+);
 
 const App = () => {
-  return (
-    <>
-    {/* <!-- Components --> */}
-    <Navbar/>
-    <Hero/>
-    <HomeCards/>
-    <JobListings/>
-    <ViewAllJobs/>
-    
-
-    </>
-  )
+  return <RouterProvider router={router}/>;
 }
 
 export default App
