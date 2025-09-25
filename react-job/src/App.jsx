aabcc43b3cef92bb92515016b500ed7cf614921a
eigenlambda123 <rmvilla987@gmail.com>
2025-09-25 17:01:56 +0800

    implemented re-routing when submiting the form with useNavigate in AddJobPage

--- react-job/src/App.jsx
+++ react-job/src/App.jsx
@@ -7,19 +7,25 @@ import JobPage, {jobLoader} from './pages/JobPage';
 import AddJobPage from './pages/addJobPage';
 
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={ <HomePage /> } />
-      <Route path="/jobs" element={ <JobsPage /> } />
-      <Route path="/add-job" element={ <AddJobPage /> } />
-      <Route path="/jobs/:id" element={ <JobPage /> } loader={jobLoader} />
-      <Route path="*" element={ <NotFoundPage /> } />
-    </Route>
-  )
-);
 
 const App = () => {
+
+  const addJob = async (newJob) => {
+  
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={ <HomePage /> } />
+        <Route path="/jobs" element={ <JobsPage /> } />
+        <Route path="/add-job" element={ <AddJobPage addJobSubmit={addJob} /> } />
+        <Route path="/jobs/:id" element={ <JobPage /> } loader={jobLoader} />
+        <Route path="*" element={ <NotFoundPage /> } />
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router}/>;
 }
 
